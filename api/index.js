9db724b462cd806ddd828db6fa51d885c6ddc36b
dilshanp <dilshanp@railtrac.net>
2025-08-31 17:46:14 +0530

    fixed vercel.json

--- api/index.js
+++ api/index.js
@@ -1,7 +1,4 @@
 // Root-level Vercel serverless function entry point
-const path = require('path');
-
-// Import the compiled app from backend
 const app = require('../backend/dist/app.js').default;
 
 module.exports = (req, res) => {
